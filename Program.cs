e9d0a7767c0a4ca69588da6a572b20eda6ccb188
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-22 15:10:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Working_with_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" This will write same line ");
+            Console.WriteLine(" This will \n wrap to next line ");
+            Console.WriteLine(" This will \" print out the quotation mark. ");
+
+            String FirstName = "Nomonde";
+            String lastName = "Zondi";
+
+            Console.WriteLine(FirstName + " " + lastName);
+
+            Console.WriteLine(lastName.Length);
+
+            Console.WriteLine(FirstName.ToUpper());
+
+            Console.WriteLine(lastName.Contains("Fu"));
+
+            Console.WriteLine(FirstName[0]);
+
+            Console.WriteLine(" Index of a is ");
+            Console.WriteLine(lastName.IndexOf('a'));
+
+            Console.WriteLine(FirstName.Substring(1));
+
+            Console.WriteLine(FirstName.Substring(1, 2));
+            Console.ReadLine();
+
+        }
+    }
+}
